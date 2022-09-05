9b7ede51c08bd5010f1837bd5acfbdae55329c9c
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-09-05 13:48:00 +0200

    Vidéo 202 : display data from Google API

--- src/containers/SearchBooks.js
+++ src/containers/SearchBooks.js
@@ -17,6 +17,45 @@ function SearchBooks() {
         dispatch(fetchBooks(title))
     }
 
+   const displayFetchedBooks = state.isLoading ? (
+        <div className='d-flex justify-content-center'>
+            <div className='spinner-boarder text-info' role="status">
+                <span className='sr-only'>Loading...</span>
+            </div>
+        </div>
+    ) : state.error !== '' ? (
+        <p>{state.error}</p>
+    ) : (
+        state.fetchedBooks.map((data) => {
+            return (
+                <div className='card mb-2' key={data.id}>
+                    <div className='card-header'>
+                        <h5 className='mb-0'>
+                            <button 
+                                className='btn btn-link collapsed'
+                                data-toggle='collapse'
+                                data-target={`#${data.id}`}
+                                aria-expanded='false'>
+                                    {data.volumeInfo.title}
+                            </button>
+                        </h5>
+                    </div>
+                    <div id={data.id} className='collapse' data-parent="#accordion">
+                        <div className='card-body'>
+                            {data.volumeInfo.hasOwnProperty('imageLinks') && <img src={data.volumeInfo.imageLinks.thumbnail} alt={data.volumeInfo.title}/>}
+                            <br />
+                            <h4 className='card-title'>Title : {data.volumeInfo.title}</h4>
+                            <h5 className='card-title'>Author : {data.volumeInfo.authors}</h5>
+                            <p className='card-text'>Description : {data.volumeInfo.description}</p>
+                            <button><a className="btn btn-outline-warning ml-6" href={data.volumeInfo.previewLink} alt="other infos of the book" target="_blank" rel="noopener noreferrer">Other infos</a></button>
+                            <button className='btn btn-outline-secondary'>Save the book</button>
+                        </div>
+                    </div>
+                </div>   
+            )
+        })
+    )
+
   return (
     <main role="main">
         <div className='jumbotron jumbotron-fluid bg-light'>
@@ -44,19 +83,12 @@ function SearchBooks() {
             </div>
         </div>
             <div className='container mt-3' style={{minHeight:'200px'}}>
-                <div className='accordion'>
-                    <div className='card mb-2'>
-                        <div className='card-header'></div>
-                        <div className='collapse' data-parent="accordion">
-                            <div className='card-body'>
-                                {}
-                            </div>
-                        </div>
-                    </div>
+                <div id='accordion'>
+                    {displayFetchedBooks}
                 </div>
             </div>
     </main>
   )
 }
 
-export default SearchBooks
+export default SearchBooks;
